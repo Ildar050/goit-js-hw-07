2e4a4f7d3f1fc22041ec851fb2abcc8a81504f41
Ildar050 <i-24@mail.ru>
2022-05-12 16:39:11 +0300

    create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery() {
+  return galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+    <img src="${preview}" alt="${description}" class="gallery__image">
+   </a>
+</div>`,
+    )
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('afterbegin', createGallery());
+
+
+
+function openOriginalImg(event) {
+  window.addEventListener('keydown', closedOriginalImg);
+  event.preventDefault();
+  const galleryItem = galleryItems.find(
+    ({ description }) => event.target.getAttribute('alt') === description,
+  );
+  const instance = basicLightbox.create(`<img src="${galleryItem.original}">`);
+  instance.show();
+}
+
+function closedOriginalImg(event) {
+  if (event.code === 'Escape') {
+    document.querySelector('.basicLightbox').remove();
+  }
+  window.removeEventListener('keydown', closedOriginalImg);
+}
+
+galleryContainer.addEventListener('click', openOriginalImg);
